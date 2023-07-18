346ef4adfc4c26a477a45bb5a9aacaf3f8214040
ming-macos <mengmengxiao@msn.com>
2023-07-18 18:02:10 +0800

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,26 +1,7 @@
 // create web server
 const express = require('express');
-const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
-const app = express();
-app.use(bodyParser.json());
-const cors = require('cors');
-app.use(cors());
-
-const commentsByPostId = {};
-
-app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
-});
-app.post('/posts/:id/comments', (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const { content } = req.body;
-    const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content });
-    commentsByPostId[req.params.id] = comments;
-    res.status(201).send(comments);
-});
-
-app.listen(4001, () => {
-    console.log('Listening on 4001');
-});
+const router = express.Router();
+// create a new comment
+router.post('/', (req, res) => {
+  const comment = new Comment({
+    text: req.body.text,
